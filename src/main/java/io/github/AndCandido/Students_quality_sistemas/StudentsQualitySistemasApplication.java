acdc9ee3abe0701097204eeda5ddc52e162c6411
André <andremesquitacandido@gmail.com>
2025-03-06 17:09:52 -0300

    initial project structure

---
+++ src/main/java/io/github/AndCandido/Students_quality_sistemas/StudentsQualitySistemasApplication.java
@@ -0,0 +1,13 @@
+package io.github.AndCandido.Students_quality_sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsQualitySistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsQualitySistemasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/AndCandido/Students_quality_sistemas/StudentsQualitySistemasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.AndCandido.Students_quality_sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsQualitySistemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
