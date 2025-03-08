7dc85ba4c0b0551bda82e320f1d5244f01c900a2
André <andremesquitacandido@gmail.com>
2025-03-08 09:15:26 -0300

    fix: implementa validaçcao completa para CPF

--- src/main/java/io/github/AndCandido/Students_quality_sistemas/dtos/StudentCreateDto.java
+++ src/main/java/io/github/AndCandido/Students_quality_sistemas/dtos/StudentCreateDto.java
@@ -2,7 +2,7 @@ package io.github.AndCandido.Students_quality_sistemas.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
@@ -17,7 +17,7 @@ public record StudentCreateDto(
     String phoneNumber,
     String cellPhoneNumber,
 
-    @Pattern(regexp = "([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}-[0-9]{2})|([0-9]{11})", message = "{field.cpf.invalid}")
+    @CPF(message = "{field.cpf.invalid}")
     String cpf,
 
     LocalDate birthDate
