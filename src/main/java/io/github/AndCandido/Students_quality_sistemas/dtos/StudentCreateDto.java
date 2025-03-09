10f3be4f114790f223d783011a5ffdf8e638115b
André <andremesquitacandido@gmail.com>
2025-03-09 12:34:07 -0300

    feat: adiciona validacao para que o campo birthDate aceite apenas datas passadas

--- src/main/java/io/github/AndCandido/Students_quality_sistemas/dtos/StudentCreateDto.java
+++ src/main/java/io/github/AndCandido/Students_quality_sistemas/dtos/StudentCreateDto.java
@@ -2,6 +2,7 @@ package io.github.AndCandido.Students_quality_sistemas.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
@@ -20,6 +21,7 @@ public record StudentCreateDto(
     @CPF(message = "{field.cpf.invalid}")
     String cpf,
 
+    @Past(message = "{field.birthDate.past}")
     LocalDate birthDate
 ) {
 }
